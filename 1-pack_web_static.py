71bbf0e291b08ba4ce1daa63863c68bd14e215be
Drihmia <drihmia.redouane@gmail.com>
2024-03-07 21:00:40 +0100

    try, except and return None

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,27 +10,29 @@ def do_pack():
     """a Fabric script that generates a .tgz archive from the contents
     of the web_static folder of your AirBnB Clone repo, using the
     function do_pack."""
+    try:
+        if not path.exists("versions/"):
+            mkdir("versions/")
 
-    if not path.exists("versions/"):
-        mkdir("versions/")
+        current_datetime = datetime.now()
+        time = current_datetime.strftime("%Y%m%d%H%M%S")
 
-    current_datetime = datetime.now()
-    time = current_datetime.strftime("%Y%m%d%H%M%S")
+        # archiving the files in web_static directory
+        archive_path = "versions/web_static_{}.tgz".format(time)
 
-    # archiving the files in web_static directory
-    archive_path = "versions/web_static_{}.tgz".format(time)
+        # announecement for packing
+        packing = "Packing web_static to {}".format(archive_path)
+        print(packing)
 
-    # announecement for packing
-    packing = "Packing web_static to {}".format(archive_path)
-    print(packing)
+        # executing the tar command
+        cmd_archive = "tar -cvzf {} web_static".format(archive_path)
+        local(cmd_archive)
 
-    # executing the tar command
-    cmd_archive = "tar -cvzf {} web_static".format(archive_path)
-    local(cmd_archive)
+        size = path.getsize(archive_path)
+        # announecement for being packed
+        packed = "web_static packed: {} -> {}Bytes".format(archive_path, size)
+        print(packed)
 
-    size = path.getsize(archive_path)
-    # announecement for being packed
-    packed = "web_static packed: {} -> {}Bytes".format(archive_path, size)
-    print(packed)
-
-    return archive_path
+        return archive_path
+    except Exception as f:
+        return None
