85bd8ddfbaaf01345147d5e8c5d6fe155b819e80
Drihmia <drihmia.redouane@gmail.com>
2024-03-08 00:25:07 +0100

    remove check if directory exist before creating it

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,6 @@
 from fabric.api import run, put, env
 from os import path
 
-
 env.hosts = ["100.26.168.177", "54.237.107.28"]
 env.user = "ubuntu"
 env.key_filename = "~/.ssh/school"
@@ -16,48 +15,44 @@ def do_deploy(archive_path):
 
     if not path.exists(archive_path):
         return False
-    try:
-        # isolating the file name
-        file_name = path.basename(archive_path)
 
-        # target name and file name with no exstention
-        file_name_no_ext = file_name.split(".")[0]
-        path_target = f"/data/web_static/releases/{file_name_no_ext}"
+    file_name = path.basename(archive_path)
 
-        # upload the file name to servers
-        tmp_dest = f"/tmp/{file_name}"
-        put(archive_path, "/tmp/")
+    # target name and file name with no exstention
+    file_name_no_ext = file_name.split(".")[0]
+    path_target = f"/data/web_static/releases/{file_name_no_ext}"
 
-        # creating the path if it does not exist
-        if not path.exists(""):
-            run_mkdir_cmd = f"mkdir -p {path_target}"
-            run(run_mkdir_cmd)
+    # upload the file name to servers
+    tmp_dest = f"/tmp/{file_name}"
+    put(archive_path, "/tmp/")
 
-        # mouve
-        # Uncompress the archive to the folder
-        # /data/web_static/releases/ <archive filename without extension>
-        # on the web server
-        run_tar_cmd = f"tar -xzf /tmp/{file_name} -C {path_target}"
-        run(run_tar_cmd)
+    # creating the path if it does not exist
+    run_mkdir_cmd = f"mkdir -p {path_target}"
+    run(run_mkdir_cmd)
 
-        # remove the temporary file from /tmp/ directory in the server(s)
-        run(f"rm {tmp_dest}")
+    # mouve
+    # Uncompress the archive to the folder
+    # /data/web_static/releases/ <archive filename without extension>
+    # on the web server
+    run_tar_cmd = f"tar -xzf /tmp/{file_name} -C {path_target}"
+    run(run_tar_cmd)
 
-        # mv the content from target/web_static to target/
-        run(f"mv {path_target}/web_static/* {path_target}")
+    # remove the temporary file from /tmp/ directory in the server(s)
+    run(f"rm {tmp_dest}")
 
-        # remove the old directory
-        run(f"rm -rf {path_target}/web_static")
-        # remove the symbolic link
-        smblc_link = "/data/web_static/current"
-        run("rm -rf {}".format(smblc_link))
+    # mv the content from target/web_static to target/
+    run(f"mv {path_target}/web_static/* {path_target}")
 
-        # recreate symblic link
-        run(f"ln -sf {path_target} {smblc_link}")
+    # remove the old directory
+    run(f"rm -rf {path_target}/web_static")
+    # remove the symbolic link
+    smblc_link = "/data/web_static/current"
+    run("rm -rf {}".format(smblc_link))
 
-        # reload nginx so the new changes will take effect
-        # run("sudo nginx -s reload")
+    # recreate symblic link
+    run(f"ln -sf {path_target} {smblc_link}")
 
-        return True
-    except Exception as f:
-        return False
+    # reload nginx so the new changes will take effect
+    # run("sudo nginx -s reload")
+
+    return True
