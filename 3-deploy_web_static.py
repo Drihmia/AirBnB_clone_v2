b1a090d3bb78aae97810c60cba68096f52175423
Drihmia <drihmia.redouane@gmail.com>
2024-03-08 00:10:22 +0100

    moved the place of import for do pack and do deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ This module contains the deploy function using Fabric module"""
 from fabric.api import env
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 env.hosts = ["100.26.168.177", "54.237.107.28"]
 env.user = "ubuntu"
@@ -13,7 +11,9 @@ def deploy():
     """a Fabric script (based on the file 2-do_deploy_web_static.py) that
     creates and distributes an archive to your web servers, using the function
     deploy"""
-    
+
+    do_pack = __import__('1-pack_web_static').do_pack
+    do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
     # pack
     archive_path = do_pack()
