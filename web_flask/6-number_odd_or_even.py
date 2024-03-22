446772fd8114a6c9069f15fafafebdb4f29b1fb2
Drihmia <138697599+Drihmia@users.noreply.github.com>
2024-03-22 08:08:35 +0000

    Update 6-number_odd_or_even.py
    remove escape totally from file for task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -16,7 +16,7 @@
         an integer:
             -> H1 tag: “Number: n is even|odd” inside the tag BODY
         """
-from flask import Flask, render_template, escape
+from flask import Flask, render_template
 
 hbnb = Flask(__name__)
 
@@ -39,14 +39,14 @@ def hello_hbnb():
 @hbnb.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """display “C ” followed by the value of the text variable"""
-    return "C {}".format(escape(text).replace("_", " "))
+    return "C {}".format(text.replace("_", " "))
 
 
 # task 3.
 @hbnb.route("/python/<text>", strict_slashes=False)
 def python_text(text="is cool"):
     """display “Python ”, followed by the value of the text variable"""
-    return "python {}".format(escape(text).replace("_", " "))
+    return "python {}".format(text.replace("_", " "))
 
 
 # task 3.
@@ -60,7 +60,7 @@ def python_is_cool():
 @hbnb.route("/number/<int:n>", strict_slashes=False)
 def number_n(n):
     """display “n is a number” only if n is an integer"""
-    return "{} is a number".format(escape(n))
+    return "{} is a number".format(n)
 
 
 # task 5.
