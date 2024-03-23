7bb9119a5d339b401606be3a6e99f3ea900d1c60
Drihmia <drihmia.redouane@gmail.com>
2024-03-23 18:36:03 +0000

    using app instead of hbhn and importing flask first

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
-HBNB = Flask(__name__)
+app = Flask(__name__)
 
 
 # task 8.
-@HBNB.teardown_appcontext
+@app.teardown_appcontext
 def teardown(_=None):
     """closing the storage and reload"""
     storage.close()
 
 
 # task 8.
-@HBNB.route("/states_list", strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
     """states list that return a list of states using 'render
     template' and jinja"""
@@ -25,4 +25,4 @@ def states_list():
 
 if __name__ == "__main__":
     """Entry point"""
-    HBNB.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
