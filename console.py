24fe3d0738a6b00e8718c16d9f805da98e5c8e10
Drihmia <drihmia.redouane@gmail.com>
2024-03-23 00:09:13 +0000

    e

--- console.py
+++ console.py
@@ -17,7 +17,7 @@ classes = {
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Contains the functionality for the HBNB console"""
+    """ Contains the functionality for the HBNB console """
 
     # determines prompt for interactive/non-interactive modes
     prompt = '(hbnb) '
