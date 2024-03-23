1d031a63419abf4b50015c47747270439509d94a
Drihmia <drihmia.redouane@gmail.com>
2024-03-23 01:45:26 +0000

    task 7 add teardwon decorator and have same out put as the example

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,63 +1,28 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application"""
 from models import storage
-from flask import Flask, render_template, request
+from models.state import State
+from flask import Flask, render_template
 
 
 HBNB = Flask(__name__)
 
 
+# task 8.
 @HBNB.route("/states_list", strict_slashes=False)
 def states_list():
-    from models.state import State
+    """states list"""
     states = storage.all(State)
     return render_template("7-states_list.html", States=states)
 
 
-with HBNB.test_request_context():
-    print("----------------")
-    print(storage.all())
-    print("----------------")
-@HBNB.route("/test")
-def test():
-    return """<!DOCTYPE html>
-<HTML lang="en">
-    <HEAD>
-        <TITLE>HBNB</TITLE>
-    </HEAD>
-    <BODY>
-        <H1>States</H1>
-        <UL>
+# task 8.
+@HBNB.teardown_appcontext
+def storage_close(error=None):
+    """closing the storage and reload"""
+    storage.close()
 
-            <LI>421a55f4-7d82-47d9-b54c-a76916479545: <B>Alabama</B></LI>
-
-            <LI>421a55f4-7d82-47d9-b54c-a76916479546: <B>Arizona</B></LI>
-
-            <LI>421a55f4-7d82-47d9-b54c-a76916479547: <B>California</B></LI>
-
-            <LI>421a55f4-7d82-47d9-b54c-a76916479548: <B>Colorado</B></LI>
-
-            <LI>421a55f4-7d82-47d9-b54c-a76916479549: <B>Florida</B></LI>
-
-            <LI>421a55f4-7d82-47d9-b54c-a76916479550: <B>Georgia</B></LI>
-
-            <LI>421a55f4-7d82-47d9-b54c-a76916479551: <B>Hawaii</B></LI>
-
-            <LI>421a55f4-7d82-47d9-b54c-a76916479552: <B>Illinois</B></LI>
-
-            <LI>421a55f4-7d82-47d9-b54c-a76916479553: <B>Indiana</B></LI>
-
-            <LI>421a55f4-7d82-47d9-b54c-a76916479554: <B>Louisiana</B></LI>
-
-            <LI>421a55f4-7d82-47d9-b54c-a76916479555: <B>Minnesota</B></LI>
-
-            <LI>421a55f4-7d82-47d9-b54c-a76916479556: <B>Mississippi</B></LI>
-
-            <LI>421a55f4-7d82-47d9-b54c-a76916479557: <B>Oregon</B></LI>
-
-        </UL>
-    </BODY>
-</HTML>"""
 
 if __name__ == "__main__":
-    HBNB.run(host="0.0.0.0", port=5000 ,debug=True)
+    """Entry point"""
+    HBNB.run(host="0.0.0.0", port=5000)
