cada3210314e2ea10f37978127432b2fd8fff88e
Drihmia <drihmia.redouane@gmail.com>
2024-03-23 21:56:54 +0000

    put back as it was

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application"""
-from flask import Flask, render_template
 from models import storage
 from models.state import State
-app = Flask(__name__)
+from flask import Flask, render_template
+
+
+HBNB = Flask(__name__)
 
 
 # task 8.
-@app.teardown_appcontext
+@HBNB.teardown_appcontext
 def teardown(_=None):
     """closing the storage and reload"""
     storage.close()
 
 
 # task 8.
-@app.route("/states_list", strict_slashes=False)
+@HBNB.route("/states_list", strict_slashes=False)
 def states_list():
     """states list that return a list of states using 'render
     template' and jinja"""
@@ -25,4 +27,4 @@ def states_list():
 
 if __name__ == "__main__":
     """Entry point"""
-    app.run(host="0.0.0.0", port=5000)
+    HBNB.run(host="0.0.0.0", port=5000)
