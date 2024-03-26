9e75c5e8a507d013d2203f8c4ad7bac0738d2738
Drihmia <drihmia.redouane@gmail.com>
2024-03-26 03:57:20 +0000

    change thee order how class attributes appears in the state

--- models/state.py
+++ models/state.py
@@ -11,9 +11,9 @@ class State(BaseModel, Base):
     __tablename__ = "states"
 
     if environ.get("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
                               cascade="all, delete-orphan")
+        name = Column(String(128), nullable=False)
     else:
         name = ""
 
